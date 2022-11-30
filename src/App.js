3c3f3ade7a072b37134d449af67d5cc45854ec75
ramyakarna <ramyakarna1216@gmail.com>
2022-11-30 12:04:50 +0530

    props example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/*import ClassComponent from './ClassComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ClassComponent2 from './ClassComponent2'
+function Demo(){
+  return(
+    <section>
+        <p>Good morning</p>
+        <h1>This is h1 tag</h1>
+        <img src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg" alt=""/>
+        <ClassComponent/>
+        <ClassComponent2/>
+    </section>    
+  )
 }
 
-export default App;
+export default Demo;*/
+import PropsExample from './PropsExample'
+function Demo(){
+  return(
+      <div>
+          <h1>This is props example</h1>
+          <PropsExample name="ramya" age="20"/>
+      </div>
+    )
+}
+
+export default Demo
---
+++ src/ClassComponent2.js
@@ -0,0 +1,12 @@
+import React from "react"
+class ClassComponent2 extends React.Component{
+	render(){
+		return(
+			<div>
+				From class component2
+			</div>
+		)
+	}
+}
+
+export default ClassComponent2
