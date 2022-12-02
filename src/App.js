3325da31be0038e30823607f658191ac5407e22e
ramyakarna <ramyakarna1216@gmail.com>
2022-12-02 11:50:31 +0530

    Events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /*import PropsExample from './PropsExample'
-import FuncPropExample from './FuncPropExample'*/
-import StateExample from './StateExample'
+import FuncPropExample from './FuncPropExample'
+import StateExample from './StateExample'*/
+import Events from './Events'
 /*import ClassComponent from './ClassComponent'
 import ClassComponent2 from './ClassComponent2'
 function Demo(){
@@ -18,6 +19,7 @@ export default Demo;*/
 function Demo(){
   return(
       <div>
+          <Events/>
           {/*<h1>This is props example</h1>
           <PropsExample name="ramya" age="20"/>
           <PropsExample framework="AngularJS" year="2008"/>
@@ -26,8 +28,8 @@ function Demo(){
           <FuncPropExample framework="react" number="2"/>
           <FuncPropExample framework="react1" number="23"/>
           <FuncPropExample framework="react2" number="2"/>
-          <FuncPropExample framework="react3" number="4"/>*/}
-          <StateExample/>     
+          <FuncPropExample framework="react3" number="4"/>
+          <StateExample/>*/}   
       </div>
     )
 }
