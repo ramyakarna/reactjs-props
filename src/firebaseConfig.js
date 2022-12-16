e32c5fbfec9ae926e0dca2b071b6c27329531d73
ramyakarna <ramyakarna1216@gmail.com>
2022-12-16 12:08:35 +0530

    authentication

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import firebase from "firebase"
+
+const config={
+  apiKey: "AIzaSyBGpkrLWqIdr94-cjCFhJElmXv_7ovCGio",
+  authDomain: "auth-12564.firebaseapp.com",
+  projectId: "auth-12564",
+  storageBucket: "auth-12564.appspot.com",
+  messagingSenderId: "1078239197809",
+  appId: "1:1078239197809:web:e9df6186291fd0f6dbf9d0"
+}
+
+firebase.initializeApp(config);
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Demo from './App';
+import Auth from './Auth';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Demo />
+    <Auth />
 );
 
 // If you want to start measuring performance in your app, pass a function
